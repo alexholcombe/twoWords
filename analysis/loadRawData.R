40d332fe4c3c0f47f5882653a0241fa64c829d00
Alex O. Holcombe <aoholcombe@gmail.com>
2017-07-17 11:50:02 +1000

    first correlogram

--- analysis/loadRawData.R
+++ analysis/loadRawData.R
@@ -22,6 +22,29 @@ for (fileName in filePaths) #Loop through and read in each Cheryl-prepared data 
   df <- rbind(df,dataThisSubject) #Add subjects data to main dataFrame
 }
 
+library(ggplot2)
+
+
+#sanity check
+g=ggplot(df,   aes(x=responsePosRelativeright))  
+g<-g+facet_grid(wordEcc~.)  +geom_histogram(bin_width=1)
+g #looks good
+
+library(dplyr)
+#ggplot will accumulate data automatically to make a histogram, will it not accumulate to make a heatPlot?
+
+dCount<- df %>% count(responsePosRelativeleft, responsePosRelativeright)
+
+correl<- ggplot(dCount, aes(x = responsePosRelativeleft, y = responsePosRelativeright)) + geom_tile(aes(fill = n)) +  facet_grid(wordEcc~.)
+show(correl)
+#theme(axis.title.y = element_blank())
+
+
+#melt data so that SPE
+
+
+ggplot(mat.melted, aes(x = SPEleft, y = SPEright, fill = value)) + geom_tile()  
+
 
 sanityCheck=FALSE
 if (sanityCheck) {
